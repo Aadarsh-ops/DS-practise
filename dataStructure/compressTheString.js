76eb8cac44189606b53618bd270a196e1d7e3584
AadarshSingh1 <>
2021-11-13 10:22:11 +0000

    compesing the string in js

---
+++ dataStructure/compressTheString.js
@@ -0,0 +1,22 @@
+ let s="aaaabbbccd";
+ let size =s.length;
+compressTheString(s,size);
+
+function compressTheString(s,size){
+  let count=1;
+  for(let i=0;i<s.length;i++){
+    let temp=s[i];
+    if(temp == s[i+1]){
+     let j=i+1;
+    while(temp==s[j]){
+      count++;
+      j++;
+    }
+    s = s.slice(0,i+1)+ count.toString() + s.slice(j,s.length);
+    console.log(i);
+    console.log(j);
+      count=1;
+    }
+  }
+  console.log(s);
+}
---
+++ dataStructure/hoghestOccuringcharacter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void higheshOccuringCharacter(string &s, int size){
+unordered_map<string, int> wordFreq;
+
+cout<s;
+ 
+ for(int i=0;i<size;i++){
+
+ }
+}
+
+int main() {
+
+  string s="aasdsdasfdfdsf";
+  int size= strlen(s);
+  higheshOccuringCharacter(s,size);
+  std::cout << "Hello World!\n";
+
+}
