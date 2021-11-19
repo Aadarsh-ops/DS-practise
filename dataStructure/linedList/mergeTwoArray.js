c69fb3c00fb8f74c6b6fb085b4d62649f8214d35
Aadarsh Singh <31269865+Aadarsh-ops@users.noreply.github.com>
2021-11-19 16:35:05 +0000

    added program for find leaders in array

--- dataStructure/linedList/mergeTwoArray.js
+++ dataStructure/linedList/mergeTwoArray.js
@@ -6,7 +6,6 @@ var mergeTwoLists = function(l1, l2) {
     
     while (l1 && l2 ){
       if(l1.val <= l2.val){
-          
               list.next = l1;
               l1 = l1.next;
            } else {
